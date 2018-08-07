639f4f4fdeed7a26a896c6f60e53dac0234ceb17
mrjay <mrjay42@gmail.com>
2018-08-07 16:40:36 +0200

    test directory

--- src/main.py
+++ src/main.py
@@ -1,35 +1,24 @@
+import numpy as np
 import cv2
-import sys
+import time
+cap = cv2.VideoCapture(0)
 
-#cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier("face")
+while(True):
 
-video_capture = cv2.VideoCapture(0)
-
-while True:
+ 
+# Wait for 5 seconds
+    time.sleep(0.01)
     # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    ret, frame = cap.read()
 
+    # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE
-    )
-
-    # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
     # Display the resulting frame
-    cv2.imshow('Video', frame)
-
+    cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything is done, release the capture
-video_capture.release()
+# When everything done, release the capture
+cap.release()
 cv2.destroyAllWindows()
